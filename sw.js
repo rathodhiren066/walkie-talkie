e47a8ca9286c2de32f80ec0aaca2465f0391980a
rathodhiren066 <rathodhiren066@gmail.com>
2026-09-11 02:03:22 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'walkie-v1';
+const FILES = ['./','./index.html','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(x => x !== CACHE_NAME ? caches.delete(x) : null))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
